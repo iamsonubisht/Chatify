5491b2171dad0706fcc5908968abb601f4b60caa
iamsonubisht <148837410+iamsonubisht@users.noreply.github.com>
2024-11-30 07:40:42 +0530

    Add files via upload

---
+++ chatify-main/client/vite.config.js
@@ -0,0 +1,31 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+
+export default defineConfig({
+  plugins: [react()],
+  server: {
+    proxy: {
+      "/api": {
+        target: "https://chatify-mern-chat-app.onrender.com",
+        changeOrigin: true,
+        secure: false,
+      },
+    },
+  },
+  build: {
+    chunkSizeWarningLimit: 1000,
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes("node_modules")) {
+            return id
+              .toString()
+              .split("node_modules/")[1]
+              .split("/")[0]
+              .toString();
+          }
+        },
+      },
+    },
+  },
+});
